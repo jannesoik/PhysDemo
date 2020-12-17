f77e0e8d4820c00d51d103a42efee2d0de618aa6
pullaposki7 <janne.soikkeli@edu.turkuamk.fi>
2020-12-17 11:30:30 +0200

    ennen prop firen lisäämistä

---
+++ Assets/Scripts/WaterSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSpawner : MonoBehaviour
+{
+    public GameObject waterParticlePrefab;
+    public Transform spawnLocation;
+    public bool tapOn;
+    public float spawntime = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tapOn = false;
+    }
+
+    
+
+    public void toggleTap()
+    {
+        tapOn = !tapOn;
+
+        if (tapOn)
+        {
+            InvokeRepeating("SpawnParticle", 0f, spawntime);
+        }
+        else
+        {
+            CancelInvoke("SpawnParticle");
+        }
+    }
+
+    public void SpawnParticle()
+    {
+        Instantiate(waterParticlePrefab, spawnLocation);
+    }
+}
